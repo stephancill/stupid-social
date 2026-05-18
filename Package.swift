5cae3d9a8a64b949cd73ad96d73c3e2b0b913cc5
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2026-05-18 10:55:40 +0200

    fix: lower iOS deployment target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "NoFeedSocial",
     platforms: [
-        .iOS(.v26),
+        .iOS(.v18),
         .macOS(.v26),
     ],
     products: [
